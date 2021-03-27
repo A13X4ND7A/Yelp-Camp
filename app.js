6ad45074b9c103715b39c6e3f2b01a6fc6581473
Alexandra <alex@faller.us>
2021-03-27 16:50:24 +0000

    added in error handling on both client side (form validations) and then server side using joi

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const path = require("path");
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const joi = require('joi');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const campground = require('./models/campground');
+const Joi = require('joi');
 
 //CONNECTING TO THE DATABASE
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -35,42 +39,71 @@ app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get("/campgrounds", async (req, res) => {
+app.get("/campgrounds", catchAsync(async (req, res) => {
  const campgrounds = await Campground.find({}); // finds all the campgrounds
  res.render('campgrounds/index', { campgrounds }) //renders a list of all the campgrounds on the index page
-});
+}));
 
 app.get('/campgrounds/new', (req, res) => {
   res.render('campgrounds/new'); //renders the page for the new campground
 });
 
-app.post('/campgrounds', async (req,res) => {
-    const campground = new Campground(req.body.campground)//addns the new campground to the db
+app.post('/campgrounds', catchAsync(async (req,res, next) => {
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    })
+    const { error } = campgroundSchema.validate(req.body);
+   if(error){
+       const msg = error.details.map(el => el.message).join(',')
+       throw new ExpressError(msg, 400);
+
+   }
+    const campground = new Campground(req.body.campground)
     await campground.save(); //saves the new campground to the db
     res.redirect(`/campgrounds/${campground._id}`)//redirects is to the campground id that was just created
-})
-app.get('/campgrounds/:id', async (req, res) => {
+}));
+
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id); //find the campground by the id
     res.render('campgrounds/show', { campground }); //outputs the campground specific on the show page
 
-});
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id); //find the campground by the id
   res.render("campgrounds/edit", { campground }); //outputs the campground specific to be edited
-});
+}));
 
-app.put('/campgrounds/:id', async(req,res) => {
+//A PUT REQUEST (METHOD-OVERRIDE ALLOWS US TO USE THIS) TO FIND THE REQUIRED CAMPGROUND AND UPDATE IT.
+app.put('/campgrounds/:id', catchAsync(async(req,res) => {
     const {id} = req.params
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
+//ALLOWS THE USER TO DELETE THE CAMPGROUND AND REDIRECTS BACK TO THE MAIN ALL CAMPGROUNDS PAGE
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-});
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'oh no something went wrong'
+    res.status(statusCode).render('error', { err });
+})
 
 app.listen(3000, () => {
     console.log('running on port 3000');
