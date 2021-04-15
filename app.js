aebdec92f2bdb5295331ca43127de5e1c8d32726
Alexandra <alex@faller.us>
2021-04-15 13:14:57 +0100

    added functionality to add reviews

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 
 
@@ -74,6 +75,8 @@ app.get('/campgrounds/:id', catchAsync(async (req, res) => {
 
 }));
 
+
+
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id); //find the campground by the id
   res.render("campgrounds/edit", { campground }); //outputs the campground specific to be edited
@@ -97,6 +100,18 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }));
+
+app.post("/campgrounds/:id/reviews/",catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
